aa2d89acc4c7425d8a7950cd75191c3144234ece
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2024-03-08 19:34:07 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
---
+++ user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    name:string,
+    userId:string,
+    password:string,
+    confirmPassword:string,
+    dateOfBirth:string,
+    gender:string,
+    country:string,
+    address:string,
+    pincode:string,
+    mobileNumber:string,
+    email:string
+  }
